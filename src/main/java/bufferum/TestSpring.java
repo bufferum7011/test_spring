a4aa2d78e8b28deb5f93f1dfa8651616ebfd00c5
bufferum7011 <bufferum@yandex.ru>
2023-05-28 01:59:46 +0300

    Внедрение зависимости

--- src/main/java/bufferum/TestSpring.java
+++ src/main/java/bufferum/TestSpring.java
@@ -3,11 +3,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        // Dependency Injection - DI - Внедрение зависимости
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
