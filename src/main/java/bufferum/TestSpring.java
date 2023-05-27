787e4e942cd095c2aeae5e300d091347ae1a92d8
bufferum7011 <bufferum@yandex.ru>
2023-05-28 02:37:57 +0300

    Setters

--- src/main/java/bufferum/TestSpring.java
+++ src/main/java/bufferum/TestSpring.java
@@ -1,8 +1,19 @@
 package bufferum;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
+    // get properties
+    // private static Properties properties = new Properties();
+    // public static String get_prop(String code) { return properties.getProperty(code); }
+
     public static void main(String[] args) {
+
+        // get properties
+        // try { properties.load(new FileInputStream("resources//application.properties")); }
+        // catch(IOException e) { System.out.println("[properties - ERROR]"); }
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
@@ -10,6 +21,14 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
+
+        // Вот что делает спринг за кулисам с сеттером
+        // MusicPlayer musicPlayer2 = new MusicPlayer();
+        // musicPlayer2.setMusic(music);
+
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
